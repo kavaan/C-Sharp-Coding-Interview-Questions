029fa7581405bea93e2d6cbbe7b00cc81a72c753
Keivan Damirchi <kavaandamirchi@gmail.com>
2023-04-14 14:58:33 +0330

    Add files via upload
    add anagrams files

---
+++ Anagram_Method_1.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    static bool AreAnagrams(string word1, string word2)
+    {
+        var word1ToChar = word1.ToCharArray();
+        var word2ToChar = word2.ToCharArray();
+
+        Array.Sort(word1ToChar);
+        Array.Sort(word2ToChar);
+
+        return word1ToChar.SequenceEqual(word2ToChar);
+    }
+
+    private static void Main(string[] args)
+    {
+        var word1 = "ali";
+        var word2 = "ila";
+        var checkAnagram = AreAnagrams(word1, word2);
+        Console.WriteLine($"Is Anagram: {checkAnagram}");
+    }
+}
